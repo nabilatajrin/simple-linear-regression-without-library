703a456ce6d82dc04b852dcdcb22f72350f35113
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-24 10:38:49 +0600

    Update lr-1d.py

--- lr-1d.py
+++ lr-1d.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, division
 from builtins import range
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,34 +10,25 @@ for line in open('data-1d.csv'):
     x, y = line.split(',')
     X.append(float(x))
     Y.append(float(y))
-
 # let's turn X and Y into numpy arrays since that will be useful later
 X = np.array(X)
 Y = np.array(Y)
-
-
 # let's plot the data to see what it looks like
 plt.scatter(X, Y)
 plt.show()
-
-
 # apply the equations to calculate a and b
-
 # denominator is common
 # note: this could be more efficient if
 #       we only computed the sums and means once
 denominator = X.dot(X) - X.mean() * X.sum()
 a = ( X.dot(Y) - Y.mean()*X.sum() ) / denominator
 b = ( Y.mean() * X.dot(X) - X.mean() * X.dot(Y) ) / denominator
-
 # calculate the predicted Y
 Yhat = a*X + b
-
 # plot everything together to make sure it worked
 plt.scatter(X, Y)
 plt.plot(X, Yhat)
 plt.show()
-
 # determine how good the model is by computing the r-squared
 d1 = Y - Yhat
 d2 = Y - Y.mean()
